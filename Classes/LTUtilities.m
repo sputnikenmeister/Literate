57119d25554093900e78aff23eb58d97c90d892c
Ryan Walklin <ryan@testtoast.com>
2010-08-23 11:48:24 +1200

    Add readme and utility class, make sure Release build is universal

---
+++ Classes/LTUtilities.m
@@ -0,0 +1,15 @@
+//
+//  LTUtilities.m
+//  Literate
+//
+//  Created by Ryan Walklin on 20/08/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "LTUtilities.h"
+
+#import "LTTextView.h"
+
+@implementation LTUtilities
+
+@end

6fb10ca423b59d8560211924b75abd4a416c878a
Ryan Walklin <ryan@testtoast.com>
2010-08-23 11:48:24 +1200

    Add readme and utility class, make sure Release build is universal

---
+++ Classes/LTUtilities.h
@@ -0,0 +1,17 @@
+//
+//  LTUtilities.h
+//  Literate
+//
+//  Created by Ryan Walklin on 20/08/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class LTTextView;
+
+@interface LTUtilities : NSObject {
+
+}
+
+@end
